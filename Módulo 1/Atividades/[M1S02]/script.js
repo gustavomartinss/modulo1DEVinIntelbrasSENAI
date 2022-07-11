39be1d6fb6d5f2e044f9fafb69c4e4c4d5668228
Gustavo Martins <gustavosm98@gmail.com>
2022-07-10 21:13:41 -0300

    commit

---
+++ Módulo 1/Atividades/[M1S02]/script.js
@@ -0,0 +1,17 @@
+let nome = document.getElementById("inputNome");
+let email = document.getElementById("inputEmail");
+let descricao = document.getElementById("inputDescricao");
+
+document.getElementById("botao").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    console.log(
+        'Formulário de contato:',
+        '\nNome: ' + nome.value,
+        '\nE-mail: ' + email.value,
+        '\nDescrição: ' + descricao.value
+    );
+
+    swal('Formulário enviado com sucesso!');
+})
+
