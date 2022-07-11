1a72804310d24972ba16595729caecb7cad91895
Gustavo Martins <gustavosm98@gmail.com>
2022-07-10 22:09:41 -0300

    commit

---
+++ Módulo 1/Atividades/[M1S02]/EX09/script.js
@@ -0,0 +1,14 @@
+let exibeNumero = document.getElementById("exibeNumero");
+
+let numero = 0;
+
+function tirar() {
+    numero--
+    exibeNumero.innerHTML = numero
+}
+
+function botar () {
+    numero++
+    exibeNumero.innerHTML = numero
+}
+
